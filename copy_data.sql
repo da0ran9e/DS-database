a03907bf3fe71f1b4626fa982815c1b0ca0033be
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-27 11:29:52 +0700

    Create copy_data.sql

---
+++ copy_data.sql
@@ -0,0 +1,13 @@
+COPY Public."categories" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/categories.csv' DELIMITER ',' CSV HEADER;
+
+COPY Public."cust_hist" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/cust_hist.csv' DELIMITER ',' CSV HEADER;
+
+COPY Public."customers" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/customers.csv' DELIMITER ',' CSV HEADER;
+
+COPY Public."inventory" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/inventory.csv' DELIMITER ',' CSV HEADER;
+
+COPY Public."orderlines" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/orderlines.csv' DELIMITER ',' CSV HEADER;
+
+COPY Public."orders" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/orders.csv' DELIMITER ',' CSV HEADER;
+
+COPY Public."products" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/products.csv' DELIMITER ',' CSV HEADER;
