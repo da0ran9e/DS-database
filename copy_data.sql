4c321c1670eb7ea9b8188733188f27def9d2b6ab
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-27 17:55:38 +0700

    Update copy_data.sql

--- copy_data.sql
+++ copy_data.sql
@@ -1,13 +1,16 @@
-COPY Public."categories" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/categories.csv' DELIMITER ',' CSV HEADER;
+COPY Public."categories" FROM 'C:/Program Files/PostgreSQL/data/categories.csv' DELIMITER ',' CSV HEADER;
 
-COPY Public."cust_hist" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/cust_hist.csv' DELIMITER ',' CSV HEADER;
+COPY Public."products" FROM 'C:/Program Files/PostgreSQL/data/products.csv' DELIMITER ',' CSV HEADER;
 
-COPY Public."customers" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/customers.csv' DELIMITER ',' CSV HEADER;
+COPY Public."inventory" FROM 'C:/Program Files/PostgreSQL/data/inventory.csv' DELIMITER ',' CSV HEADER;
 
-COPY Public."inventory" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/inventory.csv' DELIMITER ',' CSV HEADER;
+COPY Public."customers" FROM 'C:/Program Files/PostgreSQL/data/customers.csv' DELIMITER ',' CSV HEADER;
 
-COPY Public."orderlines" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/orderlines.csv' DELIMITER ',' CSV HEADER;
+COPY Public."cust_hist" FROM 'C:/Program Files/PostgreSQL/data/cust_hist.csv' DELIMITER ',' CSV HEADER;
 
-COPY Public."orders" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/orders.csv' DELIMITER ',' CSV HEADER;
+COPY Public."orders" FROM 'C:/Program Files/PostgreSQL/data/orders.csv' DELIMITER ',' CSV HEADER;
 
-COPY Public."products" FROM 'C:/Users/vuduc/OneDrive/Documents/GitHub/DS-database/products.csv' DELIMITER ',' CSV HEADER;
+COPY Public."orderlines" FROM 'C:/Program Files/PostgreSQL/data/orderlines.csv' DELIMITER ',' CSV HEADER;
+
+
+
